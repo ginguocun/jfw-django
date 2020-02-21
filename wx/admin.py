fbdb9103689d2923114e2d21b28728ef259b1c00
金国群 <guoqun.jin@hotmail.com>
2020-02-21 13:26:56 +0800

    模块注册到 admin 页面

--- wx/admin.py
+++ wx/admin.py
@@ -5,22 +5,34 @@ from django.utils.translation import gettext_lazy as _
 from wx.models import *
 
 
-@admin.register(Company)
-class CompanyAdmin(admin.ModelAdmin):
-    list_display = ['company_name', 'company_code', 'company_address']
-    search_fields = ['company_name', 'company_code', 'company_address']
+@admin.register(DistrictLevel)
+class DistrictLevelAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'level']
+    search_fields = ['level']
 
 
 @admin.register(ChinaDistrict)
 class ChinaDistrictAdmin(admin.ModelAdmin):
-    list_display = ['city_code', 'ad_code', 'name']
+    list_display = ['pk', 'city_code', 'ad_code', 'name']
     search_fields = ['city_code', 'ad_code', 'name']
 
 
 @admin.register(UserLevel)
 class UserLevelAdmin(admin.ModelAdmin):
-    list_display = ['level_code', 'level_name', 'level_desc']
+    list_display = ['pk', 'level_code', 'level_name', 'level_desc']
     search_fields = ['level_code', 'level_name', 'level_desc']
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'dish_name', 'dish_desc', 'dish_price']
+    search_fields = ['dish_name', 'dish_desc']
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'company_name', 'company_code', 'company_address']
+    search_fields = ['company_name', 'company_code', 'company_address']
 
 
 @admin.register(WxUser)
@@ -40,3 +52,14 @@ class WxUserAdmin(UserAdmin):
         (_('分类信息'), {'fields': ('user_level', 'company')}),
         (_('登录信息'), {'fields': ('last_login', 'date_joined')}),
     )
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'order_code', 'order_date', 'total_price']
+    search_fields = ['order_code']
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'order', 'dish', 'item_count', 'payed_total_price', 'is_available']
