8ced5e10d081e04ed3f77e93d52eafee879845b6
金国群 <guoqun.jin@hotmail.com>
2020-02-21 09:30:19 +0800

    添加图片存储功能

--- wx/views.py
+++ wx/views.py
@@ -8,7 +8,6 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.status import *
 from rest_framework.generics import ListCreateAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView
