50d140c28ac5a2a63fc575d158bedd261cc9a8a8
金国群 <guoqun.jin@hotmail.com>
2020-02-24 13:33:32 +0800

    数据返回更新

--- jfw/middlewares.py
+++ jfw/middlewares.py
@@ -7,5 +7,4 @@ class MiddlewareHead(MiddlewareMixin):
     def process_response(request, response):
         if request:
             response['Access-Control-Allow-Origin'] = '*'
-            response['Content-Type'] = 'application/json;charset=utf-8'
         return response
